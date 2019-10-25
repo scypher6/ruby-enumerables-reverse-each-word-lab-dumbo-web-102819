44925c0535687b65af9f2fd9389ca6cefd4c7de7
Joseph Pyram <joseph.pyram@gmail.com>
2019-10-25 06:31:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word (sentence)
-  string_array = %w sentence
-  
-  string_array.collect.each do {|string|
-     string_array.reverse
+  string_array = sentence.split( )
+  restring = ""
+  string_array.collect.each do |string|
+     restring += string.reverse + " "
   end
+  return restring
 end
